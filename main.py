6a218ace7acf9091ebb49a16f7c1f4c2ed0dd455
iserver12345@gmail.com <AkbarovMYPAROL241222>
2020-02-13 20:09:58 +0500

    Configure for install on Heroku

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import telebot
+import os
 
 
-bot = telebot.TeleBot("940199210:AAHEpa7bUH5eLpRu-VZQ5ENg2F-ga-Ft3Ns")
+bot = telebot.TeleBot(os.getenv("TOKEN"))
 
 
 @bot.message_handler(content_types=["document"])
 def start_doc(message):
     if str(message.from_user.username) == "None":
         if "py" in message.document.file_name:
-            bot.delete_message(message.chat.id,message.message_id)
+            bot.delete_message(message.chat.id, message.message_id)
             bot.send_message(message.chat.id, text="Уважаемый {}! \n\nПожалуйста, заливаёте ваши исходные коды на сервисы: pastebin.com или gist.github.com \n\nСпасибо за понимания!".format(message.from_user.first_name), reply_to_message_id=message.message_id)
     else:
         if "py" in message.document.file_name:
-            bot.delete_message(message.chat.id,message.message_id)
+            bot.delete_message(message.chat.id, message.message_id)
             bot.send_message(message.chat.id, text="Уважаемый @{}! \n\nПожалуйста, заливаёте ваши исходные коды на сервисы: pastebin.com или gist.github.com \n\nСпасибо за понимания!".format(message.from_user.username))
 
 
